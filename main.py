d0339e021e8306dc16ed95460e30899b5121c5b6
Shayaan <Shayaan@macbook-pro.home>
2023-04-12 01:43:11 +0100

    update

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from math import floor
 from PIL import Image
 from sys import argv
 
-image = Image.open(argv[1]).rotate(90, Image.NEAREST, expand = 1).resize((17, 107))
-pixels = list(image.getdata())
-
 def find_k_value(pixels):
 	k = 0
 
@@ -14,7 +11,13 @@ def find_k_value(pixels):
 
 	return k * 17
 
-k = find_k_value(pixels)
+try:
+	image = Image.open(argv[1]).rotate(90, Image.NEAREST, expand = 1).resize((17, 107))
+	pixels = list(image.getdata())
+	k = find_k_value(pixels)
+except IndexError:
+	k = int(input('Enter k: '))
+
 
 def tuppers_formula(x, y):
 	return 0.5 < floor((y // 17 // 2 ** (17 * floor(x) + floor(y) % 17)) % 2)
